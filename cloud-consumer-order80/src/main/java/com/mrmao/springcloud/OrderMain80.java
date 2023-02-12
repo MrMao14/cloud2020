5dfbc7d0b2ac7cdf875047c98a2938daa4f6311e
MrMao14 <1477672272@qq.com>
2023-02-12 17:21:33 +0800

    修改Ribbon相关注解

--- cloud-consumer-order80/src/main/java/com/mrmao/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/mrmao/springcloud/OrderMain80.java
@@ -10,6 +10,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringBootApplication
 @EnableEurekaClient
+/*
+  开启Ribbon负载均衡需要开启如下的注解
+  @see com.mrmao.springcloud.config.ApplicationContextConfig
+ */
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
 
