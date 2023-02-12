bdcc62dbedd44de947d0fc9c3d1764adaa4e2621
MrMao14 <1477672272@qq.com>
2023-02-12 16:19:16 +0800

    重命名

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/mrmao/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,13 @@
+package com.mrmao.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: MrMao
+ * @Date: 2022/4/21
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentHystrixMain8001 {
+}
